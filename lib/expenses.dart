53b600cd26b20f5fcfa5d64b779d04c6b68387b2
kotomoto <jkotor@gmail.com>
2024-03-25 12:56:37 +0100

    Add model for expenses and basic list view

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,6 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,14 +13,32 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: const [
-        Text('Chart'),
-        Text('Expenses'),
-      ],
-    ));
+      body: Column(
+        children: [
+          const Text('Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          )
+        ],
+      ),
+    );
   }
 }

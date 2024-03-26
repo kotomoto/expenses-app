c5d805e538aef7b22ed1bdb0df02963e3c1d9a2f
kotomoto <jkotor@gmail.com>
2024-03-26 11:09:15 +0100

    Store new expense

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,9 +1,10 @@
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -56,6 +57,15 @@ class _NewExpenseState extends State<NewExpense> {
               ));
       return;
     }
+
+    widget.onAddExpense(Expense(
+      title: _titleController.text,
+      amount: enteredAmount,
+      date: _selectedDate!,
+      category: _selectedCategory,
+    ));
+
+    Navigator.pop(context);
   }
 
   @override
@@ -68,7 +78,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
